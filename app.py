e118cb8ad8097bf3687faa1e127f408faf79a667
krishnabhatia425-bvp <krishnabhatia425@gmail.com>
2026-07-17 13:27:32 +0530

    Add startup message

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 import numpy as np
-print("App started")
+st.write("App is starting...")
 
 st.set_page_config(
     page_title="PlantMind AI",
